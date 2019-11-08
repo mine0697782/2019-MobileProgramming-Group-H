f75d75f4fb5ccbbde3a4c793982b22672e9002ce
jmj <jmj@kookmin.ac.kr>
2019-11-08 21:25:09 +0900

    각 화면별 액티비티 추가 (빈 페이지)

---
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/ResearchResultActicity.kt
@@ -0,0 +1,12 @@
+package com.grouph.recipelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResearchResultActicity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_research_result_acticity)
+    }
+}
---
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/ResearchingListActivity.kt
@@ -0,0 +1,12 @@
+package com.grouph.recipelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResearchingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_researching_list)
+    }
+}
