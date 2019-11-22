951193b6a32953cda8ec68d90b4d42dfa4b3f506
jmj <jmj@kookmin.ac.kr>
2019-11-23 01:24:38 +0900

    주석 JavaDoc 방식으로 변경 (작성자 : 정민재)

--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/activity/MainActivity.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 /**
  *  작성자 : 정민재
+ *  @author jmj
  */
 
 package com.grouph.recipelab.activity
@@ -48,15 +49,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myToolbar: Toolbar = toolbar
-        // 메인액티비티에서 네비게이션 드로워를 사용하기 위해 테마를 NoActionBar로 바꾸고 이를 대체한 커스텀 툴바 설정
-        // 타이틀을 설정해주기 위해 임시로 작성한 코드
+
+        /** 메인액티비티에서 네비게이션 드로워를 사용하기 위해 테마를 NoActionBar로 바꾸고 이를 대체한 커스텀 툴바 설정 */
+        /** 타이틀을 설정해주기 위해 임시로 작성한 코드 */
         myToolbar.title = "MainActivity"
         setSupportActionBar(myToolbar)
-        // 좌측에서 끌어서 사용하는 네비게이션 뷰를 초기화하는 부분
+
+        /** 좌측에서 끌어서 사용하는 네비게이션 뷰를 초기화하는 부분 */
         val navView: NavigationView = findViewById(R.id.nav_view)
-        // 네비게이션 메뉴의 아이템을 클릭했을때 반응하기 위한 이벤트 리스너 설정
+
+        /** 네비게이션 메뉴의 아이템을 클릭했을때 반응하기 위한 이벤트 리스너 설정 */
         navView.setNavigationItemSelectedListener(this@MainActivity)
 
+        /** 데이터베이스를 사용하기 위한 헬퍼 */
         helper = MySQLIteOpenHelper(this, dbName,null, dbVersion)
         try {
             db = helper.readableDatabase
@@ -64,31 +69,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             e.printStackTrace()
         }
 
-        // 리사이클러뷰에 데이터를 바인드해주기 위해 필요한 어댑터 생성
+        /** 리사이클러뷰에 데이터를 바인드해주기 위해 필요한 어댑터 생성 */
         adapterTop = ResearchingListAdapter(testData1, this, R.layout.item_research_list_card)
         adapterTop.notifyDataSetChanged()
         adapterBottom = ResearchingListAdapter(testData2, this, R.layout.item_research_list)
         adapterBottom.notifyDataSetChanged()
 
-        // 리사이클러뷰에 커스텀 어댑터를 설정하고, 좌우로 움직이도록 설정, 레이아웃간 구분선 추가
+        /** 리사이클러뷰에 커스텀 어댑터를 설정하고, 좌우로 움직이도록 설정 */
         rvTop = rv_list_researching
         rvTop.apply {
             adapter = adapterTop
             layoutManager = LinearLayoutManager(context ,LinearLayoutManager.HORIZONTAL, false)
-//            addItemDecoration(DividerItemDecoration(context , DividerItemDecoration.HORIZONTAL))
         }
-        // 상단의 카드 레이아웃이 화면 경계에 딱 맞춰지도록 하는 라이브러리 추가
+        /** 상단의 카드 레이아웃이 화면 경계에 딱 맞춰지도록 하는 라이브러리 추가 */
         val snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(rvTop)
 
+        /** 한 아이템이 페이지를 가득 채우는 리사이클러뷰에 아이템의 갯수와 현재 아이템의 인덱스를 표시해주는 라이브러리 */
         val indicator: CircleIndicator2 = indicator_main
         indicator.attachToRecyclerView(rvTop, snapHelper)
 
+        /** 상하로 움직이는 리사이클러뷰. */
         rvBottom = rv_list_research_finished
         rvBottom.apply {
             adapter = adapterBottom
             layoutManager = LinearLayoutManager(context ,LinearLayoutManager.VERTICAL, false)
+            /** 아이템간 구분선을 만들어주는 함수 */
             addItemDecoration(DividerItemDecoration(context , DividerItemDecoration.VERTICAL))
+            /** 상위 레이아웃의 스크롤과 중첩되는걸 방지하기 위해 스크롤 비활성화 */
             isNestedScrollingEnabled = false
         }
     }
@@ -97,12 +105,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    // 네비게이션 드로워를 생성하는 메서드
+    /** 네비게이션 드로워를 생성하는 메서드 */
     override fun onSupportNavigateUp(): Boolean {
         return true
     }
 
-    // 네비게이션 드로워의 아이템 클릭을 감지하는 이벤트 리스너
+    /** 네비게이션 드로워의 아이템 클릭을 감지하는 이벤트 리스너 */
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         Toast.makeText(this,p0.toString(),Toast.LENGTH_SHORT).show()
         var intent: Intent? = null
--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/helper/MySQLIteOpenHelper.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/helper/MySQLIteOpenHelper.kt
@@ -1,5 +1,6 @@
 /**
  *  작성자 : 정민재
+ *  @author jmj
  */
 
 package com.grouph.recipelab.helper
@@ -11,27 +12,27 @@ import android.database.sqlite.SQLiteOpenHelper
 class MySQLIteOpenHelper(context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int) :
     SQLiteOpenHelper(context, name, factory, version) {
 
-    // 데이터베이스가 존재하지 않으면 데이터베이스 생성을 위해 호출되는 함
+    /** 데이터베이스가 존재하지 않으면 데이터베이스 생성을 위해 호출되는 함수 */
     override fun onCreate(db: SQLiteDatabase?) {
-        // recipeTable 생성
+        /** recipeTable 생성 */
         var sql = "create table recipeTable(id integer primary key autoincrement, " +
                 "eleNum integer, resNum integer, recipeName varchar(45), isFinished bit, " +
                 "currentDate DATETIME, keyName1 varchar(45), keyName2 varchar(45), " +
                 "keyName3 varchar(45))"
         db?.execSQL(sql)
 
-        // researchTable 생성
+        /** researchTable 생성 */
         sql = "create table researchTable(researchNo integer primary key autoincrement, " +
                 "recipeNo integer, date DATETIME, score float)"
         db?.execSQL(sql)
 
-        // elementsTable 생성
+        /** elementsTable 생성 */
         sql = "create table elementsTable(id integer primary key autoincrement, " +
                 "researchNo integer, name varchar(45), value integer, unit varchar(45))"
         db?.execSQL(sql)
     }
 
-    // 데이터베이스의 버전이 변경되면 호출되는 함수
+    /** 데이터베이스의 버전이 변경되면 호출되는 함수 */
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
