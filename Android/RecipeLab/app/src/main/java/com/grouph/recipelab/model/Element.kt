2606603b00e82f5e51094b90147429755c8d8db6
jmj <jmj@kookmin.ac.kr>
2019-11-26 00:25:23 +0900

    (데이터베이스 변경) Element 모델 eleNo 추가

--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Element.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Element.kt
@@ -3,6 +3,7 @@ package com.grouph.recipelab.model
 class Element(
     var recipeNo: Int,
     var researchNo: Int,
+    var eleNo: Int,
     var name: String = "",
     var value: Int = 0,
     var unit: String = ""
