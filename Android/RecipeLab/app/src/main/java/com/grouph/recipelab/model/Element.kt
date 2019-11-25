f5efbee08712e1199659f402b2db9910d042353a
jmj <jmj@kookmin.ac.kr>
2019-11-26 00:19:29 +0900

    model 3종류 추가, (변경사항) Element 모델 recipeNo 추가

--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Element.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Element.kt
@@ -1,2 +1,9 @@
 package com.grouph.recipelab.model
 
+class Element(
+    var recipeNo: Int,
+    var researchNo: Int,
+    var name: String = "",
+    var value: Int = 0,
+    var unit: String = ""
+) {}
--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Research.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/model/Research.kt
@@ -1,2 +1,8 @@
 package com.grouph.recipelab.model
 
+class Research(
+    var researchNo: Int,
+    var recipeNo: Int,
+    var date: String,
+    var score: Float
+) {}
