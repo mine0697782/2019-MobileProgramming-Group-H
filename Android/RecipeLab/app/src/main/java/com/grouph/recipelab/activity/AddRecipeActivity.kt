57ebadfebf7ee108352236d52a9da586d7ae709d
jmj <jmj@kookmin.ac.kr>
2019-12-02 18:26:52 +0900

    AddRecipeActivity 요소추가 구현, 투명배경색 추가

--- Android/RecipeLab/app/src/main/java/com/grouph/recipelab/activity/AddRecipeActivity.kt
+++ Android/RecipeLab/app/src/main/java/com/grouph/recipelab/activity/AddRecipeActivity.kt
@@ -9,23 +9,47 @@ import android.content.Intent
 import android.database.sqlite.SQLiteException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.grouph.recipelab.R
+import com.grouph.recipelab.adapter.AddElementListAdapter
 import com.grouph.recipelab.helper.MySQLIteOpenHelper
 import com.grouph.recipelab.model.Recipe
 import kotlinx.android.synthetic.main.activity_add_recipe.*
-import kotlinx.android.synthetic.main.activity_test.*
 import kotlinx.android.synthetic.main.activity_test.btn_exit
 
 class AddRecipeActivity : AppCompatActivity() {
 
+    private val TAG = "AddRecipeActivity"
+
+    lateinit var btnAdd: TextView
+
     lateinit var btnExit: Button
     lateinit var editName: EditText
+    private lateinit var rv: RecyclerView
+    private lateinit var adapter: AddElementListAdapter
+
+    private val data = arrayListOf<ArrayList<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_recipe)
+
+        rv = rv_list_element_to_add
+        adapter = AddElementListAdapter(data, this)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        btnAdd = btn_add_element
+        btnAdd.setOnClickListener{
+            Log.d(TAG, "btnAdd")
+            adapter.data.add(arrayListOf<String>())
+            adapter.notifyDataSetChanged()
+        }
 
         editName = edit_recipe_name
         btnExit = btn_exit
